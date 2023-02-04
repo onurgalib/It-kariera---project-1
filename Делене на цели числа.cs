143031f79f009e2fef5ddda8cdc32455f359a198
onurgalib <123800316+onurgalib@users.noreply.github.com>
2023-02-04 11:29:17 +0200

    Add files via upload

---
+++ Делене на цели числа.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int remainder = number % secondNumber;
+            sum += remainder;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
